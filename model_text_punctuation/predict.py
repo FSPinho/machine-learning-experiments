bd6a9289483a1c674fd1e4fe197be93f5bfaf139
Felipe Pinho <felipe76857685@gmail.com>
2021-08-04 01:52:55 -0300

    Improving prediction test

--- model_text_punctuation/predict.py
+++ model_text_punctuation/predict.py
@@ -7,8 +7,9 @@ raw, X, y = text_util.load("simple")
 model: Model = load_model("model.h5")
 predictions = model.predict([X] * 3, verbose=True)
 
+phrase = ""
 for index, row in enumerate(raw.to_numpy()):
-    word = " ".join([letter for letter in row if isinstance(letter, str)])
-    prediction = predictions[index] > 0.5
-
-    print(f"{word}{'. [PERIOD]' if prediction else ''}")
+    if isinstance(row[-1], str):
+        prediction = predictions[index] > 0.5
+        phrase += f"{row[-1]}{'.' if prediction else ''} "
+print(f"Prediction:\n {phrase}")
